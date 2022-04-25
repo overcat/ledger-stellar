a5d3b88a2c92372c03924453218ed04becc146e7
Jun Luo <4catcode@gmail.com>
2022-04-25 16:13:14 +0800

    chore: rename Ledger App Boilerplate -> Ledger Stellar App

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 /*******************************************************************************
  *   Ledger Stellar App
  *   (c) 2017-2022 Ledger
- *   (c) 2021-2022 overcat
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
