4b24219c05aa7bf8f7f5850c26dbd2ebd69e54b4
overcat <4catcode@gmail.com>
2022-07-17 11:14:44 +0800

    test hash signing

--- tests_zemu/tests/main.test.ts
+++ tests_zemu/tests/main.test.ts
@@ -54,7 +54,6 @@ test.each(models)("app version ($name)", async (m) => {
   }
 });
 
-
 describe('get public key', () => {
   test.each(models)("get public key without confirmation ($name)", async (m) => {
     let sim = new Zemu(m.path);
@@ -94,24 +93,68 @@ describe('get public key', () => {
       await sim.close();
     }
   });
+  // TODO: refuse
 })
 
-function camelToFilePath(str: string) {
-  return str.replace(/([A-Z])/g, '-$1').toLowerCase();
-}
+describe('hash signing', () => {
+  test.each(models)("hash signing mode is not enabled ($name)", async (m) => {
+    let sim = new Zemu(m.path);
+    try {
+      await sim.start({ ...defaultOptions, model: m.name });
+      let transport = await sim.getTransport();
+      let str = new Str(transport);
+      const hash = Buffer.from("3389e9f0f1a65f19736cacf544c2e825313e8447f569233bb8db39aa607c8889", "hex")
+      expect(() => str.signHash("44'/148'/0'", hash)).rejects.toThrow(new Error("Hash signing not allowed. Have you enabled it in the app settings?"));
+    } finally {
+      await sim.close();
+    }
+  });
 
-function getTestCases() {
-  const casesFunction = Object.keys(testCasesFunction);
-  const cases = []
-  for (const rawCase of casesFunction) {
-    cases.push({
-      caseName: rawCase,
-      filePath: camelToFilePath(rawCase),
-      txFunction: (testCasesFunction as any)[rawCase]  // dirty hack
-    });
-  }
-  return cases;
-}
+  test.each(models)("approve ($name)", async (m) => {
+    let sim = new Zemu(m.path);
+    try {
+      await sim.start({ ...defaultOptions, model: m.name });
+      let transport = await sim.getTransport();
+      let str = new Str(transport);
+
+      // enable hash signing
+      await sim.clickRight()
+      await sim.clickBoth()
+      await sim.clickBoth()
+
+      const hash = Buffer.from("3389e9f0f1a65f19736cacf544c2e825313e8447f569233bb8db39aa607c8889", "hex")
+      const result = str.signHash("44'/148'/0'", hash)
+      await sim.waitScreenChange()
+      await sim.navigateAndCompareUntilText(".", `${m.prefix.toLowerCase()}-hash-signing-approve`, 'Approve')
+      let kp = Keypair.fromSecret("SAIYWGGWU2WMXYDSK33UBQBMBDKU4TTJVY3ZIFF24H2KQDR7RQW5KAEK")
+      expect((await result).signature).toStrictEqual(kp.sign(hash));
+    } finally {
+      await sim.close();
+    }
+  });
+
+  test.each(models)("reject ($name)", async (m) => {
+    let sim = new Zemu(m.path);
+    try {
+      await sim.start({ ...defaultOptions, model: m.name });
+      let transport = await sim.getTransport();
+      let str = new Str(transport);
+
+      // enable hash signing
+      await sim.clickRight()
+      await sim.clickBoth()
+      await sim.clickBoth()
+
+      const hash = Buffer.from("3389e9f0f1a65f19736cacf544c2e825313e8447f569233bb8db39aa607c8889", "hex")
+      expect(() => str.signHash("44'/148'/0'", hash)).rejects.toThrow(new Error("Transaction approval request was rejected"));
+
+      await sim.waitScreenChange()
+      await sim.navigateAndCompareUntilText(".", `${m.prefix.toLowerCase()}-hash-signing-reject`, 'Reject')
+    } finally {
+      await sim.close();
+    }
+  });
+})
 
 describe('transactions', () => {
   describe.each(getTestCases())('$caseName', (c) => {
@@ -142,3 +185,20 @@ describe('transactions', () => {
     });
   })
 })
+
+function camelToFilePath(str: string) {
+  return str.replace(/([A-Z])/g, '-$1').toLowerCase();
+}
+
+function getTestCases() {
+  const casesFunction = Object.keys(testCasesFunction);
+  const cases = []
+  for (const rawCase of casesFunction) {
+    cases.push({
+      caseName: rawCase,
+      filePath: camelToFilePath(rawCase),
+      txFunction: (testCasesFunction as any)[rawCase]  // dirty hack
+    });
+  }
+  return cases;
+}
