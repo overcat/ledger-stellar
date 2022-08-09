8a001508ce965a23964863cd83be46254cf63142
overcat <4catcode@gmail.com>
2022-08-09 19:08:09 +0800

    tests: add fuzz tests

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -5,3 +5,4 @@
 cmake -Btests_unit/build -Htests_unit -DFUZZ=1 -DCMAKE_C_COMPILER=clang
 make -C tests_unit/build/
 mv ./tests_unit/build/fuzz_tx $OUT/app-stellar-fuzz-tx
+mv ./tests_unit/build/*.so $OUT/
