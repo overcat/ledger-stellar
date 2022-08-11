31dc7cb4cf00621bffa2d19a3e1090a2f709ca97
Jun Luo <4catcode@gmail.com>
2022-08-11 15:56:56 +0800

    chore: remove unused var

--- src/globals.h
+++ src/globals.h
@@ -18,11 +18,6 @@ extern uint8_t G_io_seproxyhal_spi_buffer[IO_SEPROXYHAL_BUFFER_SIZE_B];
 extern uint32_t G_output_len;
 
 /**
- * Global structure to perform asynchronous UX aside IO operations.
- */
-extern ux_state_t G_ux;
-
-/**
  * Global structure with the parameters to exchange with the BOLOS UX application.
  */
 extern bolos_ux_params_t G_ux_params;
